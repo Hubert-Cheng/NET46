56a79c240021cc124e62ab5c0b4032464b5253ea
Terry Xu (Wicresoft North America) <v-tex@microsoft.com>
2018-04-18 15:39:24 +0800

    NET46

---
+++ DNet46AISite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DNet46AISite.MvcApplication" Language="C#" %>
---
+++ DNet46AISite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DNet46AISite.Startup))]
+namespace DNet46AISite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

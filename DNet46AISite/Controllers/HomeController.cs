8e343a2c68d688675c05858d413bc0d8b150491c
Terry Xu <v-tex@microsoft.com>
2018-04-25 02:01:03 -0700

    Update NET46

--- DNet46AISite/Controllers/HomeController.cs
+++ DNet46AISite/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Diagnostics;
+using System.Net;
+using System.Reflection;
 using System.Web.Mvc;
 
 namespace DNet46AISite.Controllers
@@ -10,12 +10,29 @@ namespace DNet46AISite.Controllers
     {
         public ActionResult Index()
         {
+            var req = WebRequest.Create("http://bing.com");
+            var response = req.GetResponse();
+            using (var s = response.GetResponseStream())
+            {
+                byte[] content = new byte[512];
+                s.Read(content, 0, content.Length);
+                var contentS = System.Text.Encoding.UTF8.GetString(content);
+                Console.WriteLine(contentS);
+                ViewBag.Content = contentS;
+            }
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page." + Assembly.GetExecutingAssembly().FullName;
+
+            ViewBag.Assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            ViewBag.Modules = Process.GetCurrentProcess().Modules;
+
+            ViewBag.EnvironmentVariables = Environment.GetEnvironmentVariables();
 
             return View();
         }
